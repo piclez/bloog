0f6d5c414e407fdce2575f9721a5ab832ad9fd96
Peter WD <peterwd@peterwd.(none)>
2012-07-06 15:36:38 -0700

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  
+  attr_accessor :blog, :title, :body
+
+  def initialize(attrs={})
+    attrs.each do |k,v| send("#{k}=",v) end
+  end
+  
+  def publish
+    blog.add_entry(self)
+  end
+
+  def persisted?
+    false
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = 'b16159d3f5bc5a94cee520c558f0306e8acbe1a858bde7bbcb552df8e6328bc46e8a0b7a8bf86e452aeb4992759d2c130c705b3a25230ad917a8bb50eeafae4e'
